78ca475ffc83aade1e66f2d9f725186c4ff7fab0
Rafey <rafeldo36@gmail.com>
2022-12-12 16:00:03 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 
 //static files
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
 app.get("/",(req, res)=>{
