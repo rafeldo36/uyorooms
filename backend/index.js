273789ceca73db15fb017b2d055d2a8b219edbea
Rafey <rafeldo36@gmail.com>
2022-12-12 16:10:04 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,16 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const cors = require('cors')
-const path = require("path");
 connectToMongo();
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.use(cors())
 app.use(express.json())
 
 //Availabe routes
 app.use('/api/auth', require('./routes/auth'))
 
-//static files
-app.use(express.static(path.join(__dirname, "../build")));
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
 
 app.get("/",(req, res)=>{
   res.json("server start");
